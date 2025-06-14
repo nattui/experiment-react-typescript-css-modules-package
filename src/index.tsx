acd4aae9f586513fc6c95c9d153ce44a52c7149f
Natt Nguyen <natt.lefevre@gmail.com>
2025-06-14 16:26:33 -0700

    tsdown

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
-export default function Test() {
-  return <div>Test</div>
+import type { ComponentProps } from "react"
+
+export interface TestProps extends ComponentProps<"div"> {}
+
+export default function Test({ ...props }: TestProps) {
+  console.log("Test")
+
+  return <div {...props}>Test</div>
 }
